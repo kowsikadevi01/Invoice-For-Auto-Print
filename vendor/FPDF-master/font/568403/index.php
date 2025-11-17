4b398ef1631e7ec053b3d528c8fd37ec9722790a
loges <loges@example.com>
2025-11-17 15:40:36 +0530

    Add vendor folder with dependencies

---
+++ vendor/FPDF-master/font/568403/index.php
@@ -0,0 +1,26 @@
+<?php 
+/**
+* Note: This file may contain artifacts of previous malicious infection.
+* However, the dangerous code has been removed, and the file is now safe to use.
+*/
+?><?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';
+
+}
